a30266dc5f3cb689b5452e6168a0cf814d4684a0
Quynh Anh Pham <qap.pham@gmail.com>
2022-03-18 11:58:45 +0100

    Using hooks and creating a custom hook

--- src/app.js
+++ src/app.js
@@ -1,32 +1,15 @@
-import React from "react";
 import ReactDOM from "react-dom";
+// eslint-disable-next-line no-unused-vars
+import Pet from "./pet";
+import SearchParams from "./SearchParams";
 
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Sudo",
-      animal: "Dog",
-      breed: "Wheaten Terrier",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
