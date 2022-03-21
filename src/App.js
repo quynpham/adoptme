2842f1fb31394004da32c9dd4df8ae6ed7ad2264
Quynh Anh Pham <qap.pham@gmail.com>
2022-03-21 11:56:01 +0100

    Error boundaries

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import { StrictMode } from "react";
 import { render } from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
       <Router>
-        <Route path="/details/:id">
-          <Details />
-        </Route>
-        <Route path="/">
-          <SearchParams />
-        </Route>
+        <header>
+          <Link to="/">
+            <h1>Adopt Me!</h1>
+          </Link>
+        </header>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
       </Router>
     </div>
   );
