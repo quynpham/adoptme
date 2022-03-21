2288b505b9ce9b8055739d5b8df1f82da309ab36
Quynh Anh Pham <qap.pham@gmail.com>
2022-03-21 09:29:52 +0100

    Adding details.js and a router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import { render } from "react-dom";
 import { StrictMode } from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <Route path="/details/:id">
+          <Details />
+        </Route>
+        <Route path="/">
+          <SearchParams />
+        </Route>
+      </Router>
     </div>
   );
 };
